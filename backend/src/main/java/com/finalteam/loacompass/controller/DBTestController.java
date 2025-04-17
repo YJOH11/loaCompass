1fdb29e24808e1b6dd837994092ffd1dfc3806b1
YJOH11 <yjohmail@naver.com>
2025-04-17 14:16:39 +0900

    DB TEST

---
+++ backend/src/main/java/com/finalteam/loacompass/controller/DBTestController.java
@@ -0,0 +1,24 @@
+package com.finalteam.loacompass.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RestController
+public class DBTestController {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/api/db-test")
+    public String testConnection() {
+        try (Connection conn = dataSource.getConnection()) {
+            return " DB 연결 성공! URL: " + conn.getMetaData().getURL();
+        } catch (Exception e) {
+            return " DB 연결 !!!!실패!!!!: " + e.getMessage();
+        }
+    }
+}
