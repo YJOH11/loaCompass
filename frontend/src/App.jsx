e15e11909a5bed16ef30bd99c0c9a0f583184835
YJOH11 <yjohmail@naver.com>
2025-04-17 16:31:49 +0900

    Test front setting+test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,24 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <h1>Hello Lost Ark</h1>;
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/db-test')
+      .then((res) => setResponse(res.data))
+      .catch((err) => {
+        console.error("API 호출 실패:", err);
+        setResponse("❌ API 호출 실패");
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>axios로 API 호출</h1>
+      <p>{response}</p>
+    </div>
+  );
 }
+
 export default App;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,7 @@
-// Vite 설정 파일
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
