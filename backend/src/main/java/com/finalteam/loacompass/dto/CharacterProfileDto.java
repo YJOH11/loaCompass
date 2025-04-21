b97e24045739d23ade6b5a861eb3f754d7596dc2
YJOH11 <yjohmail@naver.com>
2025-04-21 15:42:28 +0900

    refactor: charactorProfileDto more detail

--- backend/src/main/java/com/finalteam/loacompass/dto/CharacterProfileDto.java
+++ backend/src/main/java/com/finalteam/loacompass/dto/CharacterProfileDto.java
@@ -4,25 +4,28 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+
 public class CharacterProfileDto {
-
-    @JsonProperty("CharacterName")
     private String characterName;
-
-    @JsonProperty("CharacterClassName")
     private String characterClassName;
-
-    @JsonProperty("ItemAvgLevel")
-    private String itemAvgLevel;
-
-    @JsonProperty("ItemMaxLevel")
-    private String itemMaxLevel;
-
-    @JsonProperty("CharacterLevel")
     private int characterLevel;
-
-    @JsonProperty("ServerName")
+    private String itemAvgLevel;
+    private String itemMaxLevel;
     private String serverName;
+    private String characterImage;
+
+    private int expeditionLevel;
+    private String guildName;
+    private String guildMemberGrade;
+    private String title;
+    private String pvpGradeName;
+    private int townLevel;
+    private String townName;
+
+    private Map<String, Integer> combatStats;  // 치명, 특화 등
+    private Map<String, Integer> socialStats;  // 지성, 담력 등
 }
