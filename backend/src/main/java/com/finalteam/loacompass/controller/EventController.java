d8f9b02f5cac41535c8c2c75ffe3938789a0a24c
DD <robot9912@gmail.com>
2025-05-08 15:46:30 +0900

    로스트아크 이벤트 불러오기, footer 생성

---
+++ backend/src/main/java/com/finalteam/loacompass/controller/EventController.java
@@ -0,0 +1,25 @@
+package com.finalteam.loacompass.controller;
+
+import com.finalteam.loacompass.dto.EventDto;
+import com.finalteam.loacompass.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping
+    public ResponseEntity<List<EventDto>> getEvents() {
+        return ResponseEntity.ok(eventService.getEvents());
+    }
+}
