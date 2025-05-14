0a9d627202f5a34e151546387cda22fd81655342
DD <robot9912@gmail.com>
2025-05-14 14:23:04 +0900

    광고랑 네비게이션바 수정

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -49,10 +49,10 @@ const Navbar = () => {
                         },
                         { 
                             to: '/sassagae', 
-                            label: '사사게 게시판',
+                            label: '사사게 검색기',
                             icon: (
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd" />
+                                    <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
                                 </svg>
                             )
                         },
@@ -97,7 +97,7 @@ const Navbar = () => {
                             key={tab.to}
                             to={tab.to}
                             className={({ isActive }) =>
-                                `px-4 py-4 rounded-md text-sm font-medium flex items-center justify-center flex-1 ${
+                                `px-4 py-2 rounded-md text-sm font-medium flex items-center justify-center flex-1 ${
                                     isActive
                                         ? 'bg-gray-300 dark:bg-gray-900'
                                         : 'hover:bg-gray-300 dark:hover:bg-gray-700'
