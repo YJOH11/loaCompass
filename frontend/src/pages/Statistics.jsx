b22a25663671a5b82324ce6904a1d7f4f8f1747a
DD <robot9912@gmail.com>
2025-06-04 15:10:12 +0900

    통계 스타일 변경. 마리샵 로딩중 추가

--- frontend/src/pages/Statistics.jsx
+++ frontend/src/pages/Statistics.jsx
@@ -3,9 +3,12 @@ import StatisticsTabs from "../components/statistics/StatisticsTabs";
 
 export default function Statistics() {
   return (
-    <div className="p-6 bg-white dark:bg-gray-900 text-black dark:text-white min-h-screen">
-      {/* 제목은 StatisticsTabs로 넘김 */}
-      <StatisticsTabs title="로아 인구 통계" />
+    <div className="min-h-screen bg-transparent">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        <div className="bg-transparent">
+          <StatisticsTabs title="로아 인구 통계" />
+        </div>
+      </div>
     </div>
   );
 }
